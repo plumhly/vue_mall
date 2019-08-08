225a79f931eba8347c5684d8f5eb45248251ec56
plum <>
2019-08-09 00:04:39 +0800

    创建工程

---
+++ mall/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Router from './router'
+import 'vant/lib/icon/local.css'
+import
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
